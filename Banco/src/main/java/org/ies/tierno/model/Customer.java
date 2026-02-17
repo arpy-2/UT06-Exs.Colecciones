ac7822614d4c41940781207a6261f38ebd7fe300
arpy-2 <ghpariadna@gmail.com>
2026-02-17 09:11:18 +0100

    feat: restructure project

---
+++ Banco/src/main/java/org/ies/tierno/model/Customer.java
@@ -0,0 +1,13 @@
+package org.ies.tierno.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+    private String nif;
+    private String name;
+    private String surname;
+    private int zipCode;
+}
